41c8c524eb14425343b7f68019fc079ac0cf174d
Hena Mo <henamo@Henas-iMac.local>
2023-12-26 14:17:02 -0600

    dddfff

--- src/components/Feedback/Feadback.jsx
+++ src/components/Feedback/Feadback.jsx
@@ -2,7 +2,7 @@
 import { Container } from "./Feadback.styled"
 
 
-export const Feadback = ({ options, onLeaveFeedback, reset }) => {
+export const Feadback = ({ options, onLeaveFeedback }) => {
     return <Container>
         <div>
             {Object.keys(options).map(key => <button key={key} onClick={() => onLeaveFeedback(key)}>{key}</button>)}
